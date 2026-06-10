7f313dc09855c7e90692768ada646ff69030cc7f
5t3v3V1 <steven.nguyen.ackworth@gmail.com>
2026-06-10 13:48:42 +0100

    Implemented Dijkstra's algorithm visualiser

---
+++ DIJKSTRA/classes.py
@@ -0,0 +1,89 @@
+
+class Grid:
+    def __init__(self, layout):
+        self.layout = layout
+        self.nodes = {}
+    
+    def visualise_grid(self):
+        for row in self.layout:
+            for node in row:
+                if node.path:
+                    print("*", end = "")
+                elif node.visited or node.type == "visited_node":
+                    print("!", end = "")
+                elif node.type == "wall":
+                    print("#", end = "")
+                elif node.weight == 1:
+                    print(".", end = "")
+                elif node.weight == 5:
+                    print("?", end = "")
+                elif node.weight == 10:
+                    print("~", end = "")
+                elif node.weight == 15:
+                    print("^", end = "")
+            print()
+
+    def append_nodes(self):
+        for row_index, row in enumerate(self.layout):
+            row_items = list(row)
+            for index, item in enumerate(row_items):
+                if item == "#":
+                    node = Node("wall", (index, row_index), 0, False)
+                    self.nodes[(index, row_index)] = node
+                    row_items[index] = node
+                    self.layout[row_index] = row_items
+                elif item == ".":
+                    node = Node("node", (index, row_index), 1, False)
+                    self.nodes[(index, row_index)] = node
+                    row_items[index] = node
+                    self.layout[row_index] = row_items
+                elif item == "?":
+                    node = Node("node", (index, row_index), 5, False)
+                    self.nodes[(index, row_index)] = node
+                    row_items[index] = node
+                    self.layout[row_index] = row_items
+                elif item == "~":
+                    node = Node("node", (index, row_index), 10, False)
+                    self.nodes[(index, row_index)] = node
+                    row_items[index] = node
+                    self.layout[row_index] = row_items
+                elif item == "^":
+                    node = Node("node", (index, row_index), 15, False)
+                    self.nodes[(index, row_index)] = node
+                    row_items[index] = node
+                    self.layout[row_index] = row_items
+
+        return self.layout
+    
+    def run(self):
+        self.append_nodes()
+        self.visualise_grid()
+            
+
+class Node:
+    def __init__(self, type, position, weight, visited):
+        self.type = type
+        self.position = position
+        self.visited = visited
+        self.weight = weight
+        self.parent = None
+        self.path = False
+        self.cost = float("inf")
+
+    def get_neighbours(self):
+        x, y = self.position
+        return [(x, y - 1),
+                (x - 1, y),
+                (x, y + 1),
+                (x + 1, y)
+                ]
+    
+    def visit(self):
+        self.visited = True
+        self.type = "visited_node"
+    
+    def pathed(self):
+        self.path = True
+
+
+
---
+++ DIJKSTRA/dijkstra.py
@@ -0,0 +1,108 @@
+from classes import Grid, Node
+
+grid = Grid([
+    "????????",
+    "?^^^^^^?",
+    "?^^^^^^?",
+    "????????",
+    "?^^^^^^?"
+])
+noded_grid = grid.append_nodes()
+
+def identify_node(node_position):
+    identified_node = grid.nodes.get(node_position)
+    return identified_node
+
+def dijkstra(grid_with_nodes):
+    grid.visualise_grid()
+    for row in grid_with_nodes:
+        for column in row:
+            print(column.position, end = "")
+        print()
+
+    while True:
+        try:
+            start_position = tuple(input("Select start position: "))
+            x, y = start_position
+            m, n = int(x), int(y)
+            start_position = (m, n)
+            end_position = tuple(input("Select end position: "))
+            l, o = end_position
+            p, k = int(l), int(o)
+            end_position = (p, k)
+
+            start_node = identify_node(start_position)
+            if start_node.type == "wall":
+                print("Wall selected as start node, please reselect")
+            else:
+                break
+        except ValueError:
+            pass
+
+    unvisited = set()
+    visited = set()
+    node_costs_global = {}
+    start_node = identify_node(start_position)
+    start_node.cost = 0
+    for row in grid_with_nodes:
+        for column in row:
+            node_costs_global[column.position] = column.cost
+            unvisited.add(column.position)
+
+    while unvisited:
+        node_costs = node_costs_global.copy()
+
+        for node in list(node_costs.items()):
+            if node[0] in visited:
+                node_costs.pop(node[0])
+
+        min_cost = min(node_costs.values())
+
+        current = 0
+
+        for node in list(node_costs.items()):
+            if node[1] == min_cost:
+                current = node[0]
+
+        current_node = identify_node(current)
+        current_node.visit()
+        grid.visualise_grid()
+        print()
+
+        if current == end_position:
+            path = []
+
+            node = current_node
+
+            while node is not None:
+                path.append(node.position)
+                node.pathed()
+                node = node.parent
+    
+            grid.visualise_grid()                
+            print()
+            print(node_costs_global)
+            path.reverse()
+            print(path)
+            break
+
+        for neighbour in current_node.get_neighbours():
+            neighbour = identify_node(neighbour)
+            if neighbour is None:
+                continue
+                
+            new_cost = current_node.cost + neighbour.weight
+
+            if neighbour.type != "wall":
+                if new_cost < neighbour.cost:
+                    neighbour.parent = current_node
+                    neighbour.visit()
+                    neighbour.cost = new_cost
+                    node_costs[neighbour.position] = neighbour.cost
+                    node_costs_global[neighbour.position] = neighbour.cost
+
+        unvisited.discard(current)
+        visited.add(current)
+                    
+
+dijkstra(noded_grid)
